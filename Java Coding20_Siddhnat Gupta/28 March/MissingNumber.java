435036dac2614798cc5def89b9722520680104ea
sidgupta09 <165828853+sidgupta09@users.noreply.github.com>
2024-04-02 23:19:43 +0530

    Add files via upload

---
+++ Java Coding20_Siddhnat Gupta/28 March/MissingNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Arrays;
+class Solution {
+    public static int
+    MissingNumber(int[] arr)
+    {
+        int n=arr.length;
+        int sum1=((n)*(n+1))/2;
+        int sum2;
+        for(int i=0;i<n;i++)
+          sum2+=arr[i];
+        return sum1-sum2;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 4, 5, 6 };
+        System.out.println(MissingNumber(arr));
+    }
+}
